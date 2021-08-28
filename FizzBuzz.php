f70ab04278beb063df50ee61812f1d4b58200b72
Mario Gundlach <b.mario.gundlach@gmail.com>
2021-08-28 17:23:58 +0200

    add tests

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -12,14 +12,26 @@ class FizzBuzz
     {
         for ($i = 1; $i <= 100; $i++) {
             if (($i % 3 == 0) and ($i % 5 == 0)) {
-                echo "Fizz Buzz \n";
+                echo "Fizz Buzz ";
             } else if ($i % 3 == 0) {
-                echo "Fizz \n";
+                echo "Fizz ";
             } else if ($i % 5 == 0) {
-                echo "Buzz \n";
+                echo "Buzz ";
             } else {
-                echo $i . "\n";
+                echo $i . " ";
             }
+        }
+    }
+
+    public function getFizzBuzzForOneNumber($number) {
+        if (($number % 3 == 0) and ($number % 5 == 0)) {
+            return "Fizz Buzz";
+        } else if ($number % 3 == 0) {
+            return "Fizz";
+        } else if ($number % 5 == 0) {
+            return "Buzz";
+        } else {
+            return $number;
         }
     }
 }
--- FizzBuzz_test.php
+++ FizzBuzz_test.php
@@ -2,48 +2,49 @@
 
 class FizzBuzz_test extends PHPUnit\Framework\TestCase
 {
+
     public static function setUpBeforeClass() : void
     {
         require_once 'FizzBuzz.php';
-        $object = new FizzBuzz();
-        ob_start();
-        $result = $object->FizzBuzzOneHundred();
-        ob_get_clean();
     }
 
     public function test1() : void
     {
-        $this->assertSame('1', );
-    }
-
-    public function test2() : void
-    {
-        $this->assertSame('II', toRoman(2));
+        $object = new FizzBuzz();
+        $result = $object->getFizzBuzzForOneNumber(1);
+        $this->assertSame('1', $result);
     }
 
     public function test3() : void
     {
-        $this->assertSame('III', toRoman(3));
-    }
-
-    public function test4() : void
-    {
-        $this->assertSame('IV', toRoman(4));
+        $object = new FizzBuzz();
+        $result = $object->getFizzBuzzForOneNumber(3);
+        $this->assertSame('Fizz', $result);
     }
 
     public function test5() : void
     {
-        $this->assertSame('V', toRoman(5));
+        $object = new FizzBuzz();
+        $result = $object->getFizzBuzzForOneNumber(5);
+        $this->assertSame('Buzz', $result);
     }
 
-    public function test6() : void
+    public function test15() : void
     {
-        $this->assertSame('VI', toRoman(6));
+        $object = new FizzBuzz();
+        $result = $object->getFizzBuzzForOneNumber(15);
+        $this->assertSame('FizzBuzz', $result);
     }
 
-    public function test9() : void
+    public function testAllToHundered() : void
     {
-        $this->assertSame('IX', toRoman(9));
+        $testString = '1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 Fizz Buzz 16 17 Fizz 19 Buzz Fizz 22 23 Fizz Buzz 26 Fizz 28 29 Fizz Buzz 31 32 Fizz 34 Buzz Fizz 37 38 Fizz Buzz 41 Fizz 43 44 Fizz Buzz 46 47 Fizz 49 Buzz Fizz 52 53 Fizz Buzz 56 
+        Fizz 58 59 Fizz Buzz 61 62 Fizz 64 Buzz Fizz 67 68 Fizz Buzz 71 Fizz 73 74 Fizz Buzz 76 77 Fizz 79 Buzz Fizz 82 83 Fizz Buzz 86 Fizz 88 89 Fizz Buzz 91 92 Fizz 94 Buzz Fizz 97 98 Fizz Buzz';
+        $object = new FizzBuzz();
+        ob_start();
+        $result = $object->FizzBuzzOneHundred();
+        ob_get_clean();
+        $this->assertSame('Buzz', $result);
     }
 
 }
